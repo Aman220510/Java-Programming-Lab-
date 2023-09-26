5bf2f34720beb1de5033e6e13e154ffe6165122d
Aman220510 <143692225+Aman220510@users.noreply.github.com>
2023-09-26 12:08:47 +0530

    Add files via upload

---
+++ Arrex.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Arrex {
+
+	public static void main(String[]args) 
+	{
+	    int size;
+	    Scanner sc=new Scanner (System.in);
+	    System.out.println ("enter size");
+	    size = sc.nextInt ();
+	    for (int i = 0;i<size;i++ )
+	    {
+	        a[i]=i;
+	        
+	    }
+		
+	}
+}
